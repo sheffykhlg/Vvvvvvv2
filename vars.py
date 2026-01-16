f0b01e1e77ebfcd6690a0645759d65e1939c1a9b
sheffykhlg <56907336+sheffykhlg@users.noreply.github.com>
2026-01-16 13:19:50 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -14,7 +14,7 @@ MONGO_URL = DATABASE_URL  # For auth system
 
 # Owner and Admin Configuration
 OWNER_ID = int(os.environ.get("OWNER_ID", "7514808135"))
-ADMINS = [int(x) for x in os.environ.get("ADMINS", "8232691888").split()]  # Default to owner ID
+ADMINS = [int(x) for x in os.environ.get("ADMINS", "8232691888 7893435873").split()]  # Default to owner ID
 
 # Channel Configuration
 PREMIUM_CHANNEL = ""
@@ -59,6 +59,7 @@ Please contact the admin to get access.</blockquote>""",
 
 <blockquote>Use format: {format}</blockquote>"""
 }
+
 
 
 
